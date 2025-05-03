4692749d0bf90a0c5091da1596253b6c192bf604
Casper Hart <casperhart93@gmail.com>
2025-05-03 13:32:41 -0400

    rust version

--- src/scripts/sudoku.js
+++ src/scripts/sudoku.js
@@ -1,21 +1,3 @@
-export function gen_sudoku(wasm) {
-  const memory = wasm.instance.exports.memory;
-  const get_sudoku = wasm.instance.exports.get_sudoku;
-  const buffer_offset = get_sudoku();
-
-  const unsolved = new Uint8Array(memory.buffer, 0, 81);
-  const solved = new Uint8Array(memory.buffer, 81, 81);
-  const out1 = new TextDecoder().decode(unsolved);
-  const out2 = new TextDecoder().decode(solved);
-
-  const get_sudoku_buffer = wasm.instance.exports.get_sudoku_buffer;
-
-  const test = new Uint8Array(memory.buffer, get_sudoku_buffer(), 81 + 81 + 4);
-  console.log(new TextDecoder().decode(test));
-
-  return [out1, out2];
-}
-
 export function formatSudoku(sudoku) {
   if (sudoku.length !== 81) {
     throw new Error("Invalid Sudoku string length");
